6fee37b8579a602c55b96e4063788fe4ab4bbd3f
Maleah <111613370+xleahw@users.noreply.github.com>
2023-09-22 12:40:11 -0400

    Create lab01.c

---
+++ labs/01/lab01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("Hello World");
+
+   int i = 5;
+   char c = '@';
+   float f = 2.7;
+
+   printf("i = %d\n", i);
+   printf("c = %c\n", c);
+   printf("f = %.2f\n", f);
+
+   return EXIT_SUCCESS;
+}
